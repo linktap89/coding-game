06f116e3a7adfaaf176891fc49228feef93f3b68
Link Tapsell <linkntapsell@hotmail.com>
2021-03-23 01:03:55 +1100

    update js and page1

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-/// Countdown timer///
-function callback(){
-    document.getElementById('countdown').innerHTML="Game Over";
-}
-function start(){
-    function decrease_count(){
-        var div = getElementById('countdown');
-        if( --div.innerHTML == 0) {
-            clearInterval(timer)
-            callback();
-            }
-
-        }
-    var timer = setInterval(decrease_count, 1000);
-}
+///Quiz Buttons///
