d72d4f122c07173721903eae5f90265211e2599a
Link Tapsell <linkntapsell@hotmail.com>
2021-03-27 14:08:06 +1100

    reveal questions

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-// Global variables //
-var scoreTracker = 0;
-var resetButton = document.querySelector(".resetTestButton");
-var questionTracker = 1 
+var score = 0;
+
+
 
 // button countdown timer//
     const timeLeftDisplay = document.querySelector('#time-left')
@@ -21,59 +20,8 @@ var questionTracker = 1
     countDown()
 
 
-/***********************************************/
+/*******Question Buttons******************/
 
-// Questions + Answers from local storage//
-
-var testquestions = document.getElementById("question");
-var testanswers = document.getElementById("answer");
-var comment = document.getElementById("msg");
-var saveButton = document.getElementById("save");
-var savedAnswer = document.getElementById("savedAnswer");
-
-function saveLastAnswer() {
-    var testResult = {
-    testquestions: questions.value,
-    testanswers: answers.value,
-    comment: comment.value.trim()
-};
-// setItem to store oject in storage and JSON.stringfy to convert it as a string.//
-localStorage.setItem("testResult",JSON.stringify(testResult));
-}
-
-function renderLastAnswer () {
-    //Use JSON.parse to covert text to Javascript object
-    var lastAnswer = JSON.parse(localStorage.getItem("testResult"));
-    //Check if data returned is correct or in correct//
-    if (lastAnswer !== null){
-        document.getElementById("savedAnswer"). innerHTML = lastAnswer.answer;
-    } else {
-        return;
-    }
-}
-
-/*********************************************/
-
-//Score Calculator//Friday Class//////
-
-
-
-//// If Answer is correct////
-
-
-
-//// If Answer is incorrect////Firday Class////
-
-
-/////Timer Runs Out////
-function restartTest (){
-  resetTestButton.addEventListener("click",function (){
-    count = 60;
-
-
-
-  })
-}
 /// eventListner button///
 const questionbtns = document.querySelectorAll (".questionsbtn")
 /// For each loop ///
@@ -82,19 +30,15 @@ questionbtn.addEventListener("click",function (event){
   console.log(event.target)
   var currentbtn = event.target
   if (currentbtn.className.includes("correctAnswer")){
-    console.log("Correct Answer") 
+    console.log("Correct Answer")
+    question2.classList.toggle('active')
+    question1.classList.remove('active')
   }
   else{
-    console.log("wrong Answer!")
+    console.log("Wrong Answer!")
     timeLeft -= 10
   }
-  
-
-  
-
-
-
 })
+ })
 
-
-})
+ 
